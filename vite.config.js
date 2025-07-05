c4d7b4791a6bb1c57e334cd4b84dfcb24e5949fc
SanyaTheGreat <polikalexander@gmail.com>
2025-07-05 20:09:18 +0300

    walletupdate13

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+        'process.env': '{}',
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+      ],
+    },
+  },
+  define: {
+    'process.env': {},
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer',
+    },
+  },
   server: {
     mimeTypes: {
-      '.json': 'text/plain' // 👈 Lottie expects text/plain, not application/json
-    }
-  }
-})
+      '.json': 'text/plain', // 👈 Lottie expects text/plain
+    },
+  },
+});
