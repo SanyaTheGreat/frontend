993184a49fb09e5ceaf496b4321271b8909db571
SanyaTheGreat <polikalexander@gmail.com>
2025-07-07 18:11:57 +0300

    newtry18

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-import { Buffer } from 'buffer';
-
-window.Buffer = Buffer;
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
+import { Buffer } from 'buffer';
+
+window.Buffer = Buffer;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
