4f145f593a838c0fdc947d132f07b559c935203c
SanyaTheGreat <polikalexander@gmail.com>
2025-07-07 18:52:43 +0300

    newtry26

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx';
 import './index.css';
 import { Buffer } from 'buffer';
 
-window.Buffer = Buffer;
+globalThis.Buffer = Buffer;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
