1cb23ad5f1fe02d576ba5de91f0c90a132bd8a48
SanyaTheGreat <polikalexander@gmail.com>
2025-10-12 03:35:21 +0300

    lol2

--- src/components/TabBar.jsx
+++ src/components/TabBar.jsx
@@ -6,7 +6,7 @@ const TabBar = () => {
     <div className="tab-bar">
       <Link to="/" className="tab-link">Главная</Link>
       <Link to="/history" className="tab-link">История</Link>
-      <Link to="/spins" className="tab-btn">Крутка</Link>
+      <Link to="/spins" className="tab-btn">Ролл</Link>
       <Link to="/ingame" className="tab-link">Мои Игры</Link>
       <Link to="/profile" className="tab-link">Профиль</Link>
       <Link to="/leaderboard" className="tab-link">Топ</Link>
