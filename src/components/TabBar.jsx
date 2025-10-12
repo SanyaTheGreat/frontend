6901620b14bd0614ae0f7104a250ae8fcf3d3c3b
SanyaTheGreat <polikalexander@gmail.com>
2025-10-12 03:33:53 +0300

    lol2

--- src/components/TabBar.jsx
+++ src/components/TabBar.jsx
@@ -4,12 +4,13 @@ import './TabBar.css'; // Здесь будут стили для таббара
 const TabBar = () => {
   return (
     <div className="tab-bar">
-      <Link to="/" className="tab-link">Я</Link>
+      <Link to="/" className="tab-link">Главная</Link>
       <Link to="/history" className="tab-link">История</Link>
+      <Link to="/spins" className="tab-btn">Крутка</Link>
       <Link to="/ingame" className="tab-link">Мои Игры</Link>
       <Link to="/profile" className="tab-link">Профиль</Link>
       <Link to="/leaderboard" className="tab-link">Топ</Link>
-      <Link to="/spins" className="tab-btn">Крутка</Link>
+      
     </div>
   );
 };
